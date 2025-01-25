17833746a595c1fd9e5d97a59729f1535b4e97f1
Ubuntu <ubuntu@ip-172-31-7-2.ap-south-1.compute.internal>
2025-01-25 04:56:47 +0000

    jenkins

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, Devops Team From Skills IT \n');
+  res.end('Hello, Devops Team From Jenkins \n');
 });
 
 server.listen(PORT, () => {
